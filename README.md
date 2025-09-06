ea08e7daad7f3f9f3c0cbcd0016b287c2bdfbf31
RavirajMhalsekar <ravirajmhalsekar@gmail.com>
2025-09-06 20:03:55 +0530

    added cicd

--- README.md
+++ README.md
@@ -92,7 +92,7 @@ open up localhost:
 
 	
 ## 3. Create ECR repo to store/save docker image
-    - Save the URI: 970547337635.dkr.ecr.ap-south-1.amazonaws.com/medicalchatbot
+    - Save the URI: 734300883520.dkr.ecr.ap-south-1.amazonaws.com/medicalchatbot
 
 	
 ## 4. Create EC2 machine (Ubuntu) 
--- store_index.py
+++ store_index.py
@@ -17,15 +17,16 @@ pc = Pinecone(api_key=PINECONE_API_KEY)
 
 index_name = "medicalbot"
 
-pc.create_index(
-    name=index_name,
-    dimension=384, 
-    metric="cosine", 
-    spec=ServerlessSpec(
-        cloud="aws", 
-        region="us-east-1"
+if index_name not in pc.list_indexes().names():
+    pc.create_index(
+        name=index_name,
+        dimension=384, 
+        metric="cosine", 
+        spec=ServerlessSpec(
+            cloud="aws", 
+            region="us-east-1"
+        ) 
     ) 
-) 
 
 # Embed each chunk and upsert the embeddings into your Pinecone index.
 docsearch = PineconeVectorStore.from_documents(
